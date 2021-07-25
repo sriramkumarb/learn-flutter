9dad5c8d04496fd1c8d30b124b9872765e821095
Sriram <sriram9159541445@gmail.com>
2021-07-25 12:44:30 +0530

    icon button

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Center(child: Text('Hello Sri!')),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      body: Center(
-        child: Image(
-          image: AssetImage('images/owl.jpg'),
-          height: 250,
-          width: 150,
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            'Hello Sri!',
+            style: GoogleFonts.lato(),
+          )),
+          backgroundColor: Colors.green[600],
+        ),
+        body: Center(
+          child: ElevatedButton.icon(
+            onPressed: () {
+              print('Hi');
+            },
+            icon: Icon(Icons.mail),
+            label: Text('Mail'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.green),
+              shadowColor: MaterialStateProperty.all(Colors.green.shade100),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.green),
+              )),
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Hi');
-        },
-        child: Center(child: Text('Click')),
-      ),
     ),
-  ));
+  );
 }
